7287d222a9f7783e1237f1b6bd63b59da40efa77
Tech-A35x <tech-a35x@Tech-A35s-MacBook-Air.local>
2019-01-23 08:43:28 +0000

    progress

--- animal_classes/gorilla.rb
+++ animal_classes/gorilla.rb
@@ -1,14 +1,21 @@
-require_relative '../zoo/animal_module'
 require_relative '../mammal'
+require_relative '../zoo/animal_module'
+
 
 
 class Gorilla < Mammal
      
 #over-riding
-  
-def eat
-  Animal.eat
-end
+    
+   def speak 
+    puts 'Gorilla sounds'
+    super 
+   end
+
+   def live
+    puts 'theres a big fat gorilla lives in the jungle, but it is shilling in the zoo for now'
+    super
+  end
 
 
 end
--- zoo/animal_module.rb
+++ zoo/animal_module.rb
@@ -4,14 +4,17 @@ module Animal
      puts 'the animal lives and is a breathing creature'
    end
   
+  class Eat
    def self.eat
      puts 'like humans the animal eats to survive'
    end
-
-
+  end 
+  
+ class Speak
    def self.speak 
     puts 'they make noises but not the way humans do'
    end
+  end
 
 end
 #Animal.live
